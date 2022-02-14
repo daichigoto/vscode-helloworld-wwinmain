5022de6d82065ddb4d6e6f45697c7910c8183537
daichigoto <daichigoto@gmail.com>
2022-02-14 12:44:42 +0900

    updated

--- winmain.c
+++ winmain.c
@@ -23,12 +23,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     wc.lpszClassName = CLASS_NAME;
 
     wc.style         = CS_HREDRAW | CS_VREDRAW;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
-    wc.lpszMenuName  = NULL;
+//    wc.cbClsExtra    = 0;
+//    wc.cbWndExtra    = 0;
+//    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+//    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+//    wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
+//    wc.lpszMenuName  = NULL;
 
     RegisterClass(&wc);
 
