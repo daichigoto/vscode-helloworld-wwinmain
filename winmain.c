6db2e215b8046ac65bc515da69960c06a5760264
daichigoto <daichigoto@gmail.com>
2022-02-14 12:48:12 +0900

    updated

--- winmain.c
+++ winmain.c
@@ -22,11 +22,11 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     wc.hInstance     = hInstance;
     wc.lpszClassName = CLASS_NAME;
 
-    wc.style         = CS_HREDRAW | CS_VREDRAW;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+//    wc.style         = CS_HREDRAW | CS_VREDRAW;
+//    wc.cbClsExtra    = 0;
+//    wc.cbWndExtra    = 0;
+//    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+//    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
     wc.hbrBackground = (HBRUSH) (COLOR_WINDOW + 1);
 //    wc.lpszMenuName  = NULL;
 
